84024f411eeb3cd38cb6822f860917a0eb57ebe2
qkrvudtn98 <94512239+qkrvudtn98@users.noreply.github.com>
2021-12-10 14:38:39 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Your Vue.js App"/>
-    <!-- 컴포넌트 명명법 종류 -->
-    <!-- <hello-world></hello-world>
-    <HelloWorld></HelloWorld>
-    <HelloWorld/> -->
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  //인스턴스 옵션 속성 or 컴포넌트 옵션 속성 
-  name: 'App',
-  components: {
-    HelloWorld,
-  }
-}
 </script>
 
 <style>
