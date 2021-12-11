ca78b7ab505cc9169ae3bb75db1b4dcdf1476eb8
jemin <jemin03120111@gmail.com>
2021-12-12 02:14:06 +0900

    vue router 추가

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div>
-    <FirstPage v-bind:data="message"></FirstPage>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import FirstPage from "./components/firstPage.vue";
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "App",
-  components: {
-    FirstPage,
-  },
-  data: function () {
-    return {
-      message: "jemin",
-    };
-  },
-};
-</script>
+#nav {
+  padding: 30px;
+}
 
-<style></style>
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
